5bb2d2f269de759c741634a99bbf9327fa341755
graua-git <graua@oregonstate.edu>
2023-02-22 12:02:08 -0800

    Working game & bots, tests dont account for + and # notation

--- Game/Kevin.py
+++
@@ -1,9 +0,0 @@
-from ChessGame import *
-import random
-
-class Kevin():
-    def pick_move(self, move_list: list) -> Move:
-        """
-        Takes list of all legal moves, picks one at random
-        """
-        return random.choice(move_list)
